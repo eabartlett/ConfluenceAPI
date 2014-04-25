6939c46efa5abd8ad856f63d4a2de6806e93489f
eabartlett <eabartlett@berkeley.edu>
2014-04-25 11:55:26 -0700

    Getting audio is probably working. Not exactely sure how to test it...

--- routes/audio.js
+++ routes/audio.js
@@ -14,7 +14,21 @@ var ObjectId = mongoose.Types.ObjectId;
 var errorCallback = util.errorCallback;
 
 module.exports.get = function(db, schema){
-  return function(req, res){}
+  return function(req, res){
+    var model = (req.query.question)?schema.question:schema.answer;
+    var id = (req.query.question)?req.query.question:req.query.answer;
+    model.findById(new ObjectId(id), function(err, data){
+      console.log(data.audio);
+      var stat = fs.statSync(data.audio);
+      var file = fs.createReadStream(data.audio);
+      res.writeHeader(200, {
+        'Content-type': 'audio/mp4',
+        'Content-length': stat.size
+      });
+      file.pipe(res);
+      res.end();
+    });
+  }
 }
 
 module.exports.post = function(db, schema, uploadPath){
@@ -22,18 +36,18 @@ module.exports.post = function(db, schema, uploadPath){
     //use model.update to put path to file into the db
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files){
-      var filename = path.join(uploadPath, fields.id+'.m4a');
+      var model = (fields.question)?schema.question:schema.answer;
+      var id = (fields.question)?fields.question:fields.answer;
+      var filename = path.join(uploadPath, id+'.m4a');
+      console.log(files.audio);
       
-      schema.question.findOneAndUpdate(
-        {_id: new ObjectId(fields.id)}, {audio: filename}, function(err){}
+      model.findOneAndUpdate(
+        {_id: new ObjectId(id)}, {audio: filename}, function(err){}
       );
-      schema.question.findById(new ObjectId(fields.id), function(err, data){
-        res.end(JSON.stringify(data));
-      });
       fs.rename(files.audio.path, filename, function(err){
         if(err) res.end('Error uploading audio:\n\n' + err.toString());
       });  
-      schema.question.findById(new ObjectId(fields.id), function(err, data){
+      model.findById(new ObjectId(id), function(err, data){
         if(!data){
           res.end("Invalid question Id");
         }
